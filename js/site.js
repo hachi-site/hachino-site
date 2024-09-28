781671ff7850e3957e797535304fd97346af993d
hachi-site <hachi.git@gmail.com>
2024-09-29 01:28:08 +0900

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,6 @@
+function tik() {
+    window.location.href = 'https://www.tiktok.com/@hachida1627?is_from_webapp=1&sender_device=pc';
+}
+function you() {
+    window.location.href = 'https://youtube.com/@hachidao671?si=iqgtSXeW_6V_6fMr'
+}
